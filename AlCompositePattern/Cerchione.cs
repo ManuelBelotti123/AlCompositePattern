5bdc7ca1d4aab0aa459f5a083875badfbee02d59
manuel <manue@LENOVOPC>
2024-01-05 10:13:43 +0100

    Creazione di 2 leaf: Cerchione e Gomma, che faranno parte di Ruota

---
+++ AlCompositePattern/Cerchione.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlCompositePattern
+{
+    internal class Cerchione : Componente
+    {
+        //attributi
+        private double _pollici;
+        private string _materiale;
+
+        //proprietà
+        public double Pollici { get { return _pollici; } set { _pollici = value; } }
+        public string Materiale { get { return _materiale; } set { _materiale = value; } }
+
+        //costruttori
+        public Cerchione()
+        {
+            Pollici = 0;
+            Materiale = "";
+        }
+        public Cerchione(double pol, string mat)
+        {
+            Pollici = pol;
+            Materiale = mat;
+        }
+        public Cerchione(Cerchione cer)
+        {
+            Pollici = cer.Pollici;
+            Materiale = cer.Materiale;
+        }
+
+        //metodo equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Cerchione))
+            {
+                return false;
+            }
+            else
+            {
+                Cerchione cer = (Cerchione)obj;
+                return cer != null
+                    && Pollici == cer.Pollici
+                    && Materiale == cer.Materiale;
+            }
+        }
+
+        //metodo GetHashCode
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        //metodo ToString
+        public override string ToString()
+        {
+            return Materiale + ";" + Pollici.ToString();
+        }
+
+        //metodi
+        public override string Descrizione()
+        {
+            return "Il materiale del cerchione è " + Materiale + " e i suoi pollici sono " + Pollici.ToString() + ".";
+        }
+
+        public override double CalcoloCosto()
+        {
+            return Pollici * 2;
+        }
+    }
+}
---
+++ AlCompositePattern/Gomma.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlCompositePattern
+{
+    internal class Gomma : Componente
+    {
+        //attributi
+        private double _altezza;
+        private double _larghezza;
+        private double _raggio;
+
+        //proprietà
+        public double Altezza { get { return _altezza; } set { _altezza = value; } }
+        public double Larghezza { get { return _larghezza; } set { _larghezza = value; } }
+        public double Raggio { get { return _raggio; } set { _raggio = value; } }
+
+        //costruttori
+        public Gomma()
+        {
+            Altezza = 0;
+            Larghezza = 0;
+            Raggio = 0;
+        }
+        public Gomma(double alt, double lrg, double rag)
+        {
+            Altezza = alt;
+            Larghezza = lrg;
+            Raggio = rag;
+        }
+        public Gomma(Gomma gm)
+        {
+            Altezza = gm.Altezza;
+            Larghezza = gm.Larghezza;
+            Raggio = gm.Raggio;
+        }
+
+        //metodo equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Gomma))
+            {
+                return false;
+            }
+            else
+            {
+                Gomma gm = (Gomma)obj;
+                return gm != null
+                    && Altezza == gm.Altezza
+                    && Larghezza == gm.Larghezza
+                    && Raggio == gm.Raggio;
+            }
+        }
+
+        //metodo GetHashCode
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        //metodo ToString
+        public override string ToString()
+        {
+            return Altezza.ToString() + ";" + Larghezza.ToString() + ";" + Raggio.ToString();
+        }
+
+        //metodi
+        public override string Descrizione()
+        {
+            return "L'altezza della gomma è " + Altezza.ToString() + ", la sua larghezza è " + Larghezza.ToString() + " e il suo raggio è " + Raggio.ToString() + ".";
+        }
+
+        public override double CalcoloCosto()
+        {
+            return Altezza * Larghezza * Raggio;
+        }
+    }
+}
